5232bb8a5e5e85d8239d997b1a3abdc15c81c07b
csdevto <53869923+csdevto@users.noreply.github.com>
2019-08-08 13:46:46 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 BASE_URL='https://www.minecraft.net/en-us/download/server/bedrock/'
-SERVER=${MCSERVERFOLDER}/server
-DOWNLOAD=${MCSERVERFOLDER}/download
-BACKUP=${MCSERVERFOLDER}/backup
+SERVER=${MCFolder}/server
+DOWNLOAD=${MCFolder}/download
+BACKUP=${MCFolder}/backup
 
 #checks if folders exist
     if [ -d ${DOWNLOAD} ] ; then
-        echo  "${DOWNLOAD} already exists."
+        printf  "${DOWNLOAD} already exists."
         else
         mkdir -p ${DOWNLOAD}
-        echo  "${DOWNLOAD} created."
+        printf  "${DOWNLOAD} created."
     fi
     if [ -d ${SERVER} ] ; then
-        echo  "${SERVER} already exists."
+        printf  "${SERVER} already exists."
         else
         mkdir -p ${SERVER}
-        echo  "${SERVER} created."
+        printf  "${SERVER} created."
     fi
     if [ -d ${BACKUP} ] ; then
-        echo  "${BACKUP} already exists."
+        printf  "${BACKUP} already exists."
         else
         mkdir -p ${BACKUP}
-        echo  "${BACKUP} created."
+        printf  "${BACKUP} created."
     fi
-
+printf "\n"
 # checks if latests ver downloaded
 URL=`curl -L ${BASE_URL} 2>/dev/null| grep bin-linux | sed -e 's/.*<a href=\"\(https:.*\/bin-linux\/.*\.zip\).*/\1/'`
 if [ ! -e ${DOWNLOAD}/${URL##*/} ] ; then
@@ -32,17 +32,17 @@ if [ ! -e ${DOWNLOAD}/${URL##*/} ] ; then
   cp ${SERVER}/server.properties ${BACKUP}/server.properties.bak
   cp ${SERVER}/permissions.json ${BACKUP}/permissions.json.bak
   cp ${SERVER}/whitelist.json ${BACKUP}/whitelist.json.bak
-  echo "Backup Created"
+  printf "Backup Created."
   cd ${SERVER}
   # if not download and update server folder
   curl ${URL} --output ${DOWNLOAD}/${URL##*/}
   unzip -o ${DOWNLOAD}/${URL##*/} 2>&1 > /dev/null
-  echo "MC Bedrock Server Updated"
+  printf "MC Bedrock SERVER Updated."
   # restore backup
   cp ${BACKUP}/server.properties.bak ${SERVER}/server.properties
   cp ${BACKUP}/permissions.json.bak ${SERVER}/permissions.json
   cp ${BACKUP}/whitelist.json.bak ${SERVER}/whitelist.json
-  echo "Backup Restored"
+  printf "Backup Restored.\n"
   # Remove older copies
   find ${DOWNLOAD} -maxdepth 1 -type f -name bedrock-server\*.zip ! -newer ${DOWNLOAD}/${URL##*/} ! -name ${URL##*/} -delete
 
@@ -54,7 +54,14 @@ if [ ! -d ${SERVER}/minecraftpe ] ; then
   rm ${SERVER}/minecraftpe
 fi
 #change server properties with variables
-sed -i -e "s/=19132/=${MCPORT}/g" "${SERVER}/server.properties"
-sed -i -e "s/level-name=.*/level-name=${WORLD}/g" "${SERVER}/server.properties"
+if [ ${DockerSet}  = "YES" ]; then
+	sed -i -e "s/=19132/=${MCPort}/g" "${SERVER}/server.properties"
+	sed -i -e "s/level-name=.*/level-name=${LevelName}/g" "${SERVER}/server.properties"
+	sed -i -e "s/server-name=.*/server-name=${ServerName}/g" "${SERVER}/server.properties"
+	sed -i -e "s/gamemode=.*/gamemode=${GameMode}/g" "${SERVER}/server.properties"
+	printf "server.properties set based on Docker variables.\n"
+else
+	printf "Please edit server.properties manually.\n"
+fi
 cd ${SERVER}
 LD_LIBRARY_PATH=. ./bedrock_server
